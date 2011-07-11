6927a12768ee4b2d60267795bec015db4dec4fa8
Oscar Renalias <oscar@renalias.net>
2011-07-12 00:59:07 +0200

    Added the logback logging dependency

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -5,5 +5,6 @@ class Project(info: ProjectInfo) extends DefaultProject(info) with IdeaProject {
 val dispatch_http = "net.databinder" %% "dispatch-http" % "0.8.3" withSources()
 val dispatch_http_nio = "net.databinder" %% "dispatch-nio" % "0.8.3" withSources()
 val specs = "org.scala-tools.testing" %% "specs" % "1.6.8" % "test"
+val log_lib = "ch.qos.logback" % "logback-classic" % "0.9.26"
 
 }
