ce87de97d0fa80c946b548477674be4c263f77b6
Oscar Renalias <oscar@renalias.net>
2011-07-13 15:38:25 +0200

    Added a dependency to my own common classes for logging

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -5,6 +5,8 @@ class Project(info: ProjectInfo) extends DefaultProject(info) with IdeaProject {
 val dispatch_http = "net.databinder" %% "dispatch-http" % "0.8.3" withSources()
 val dispatch_http_nio = "net.databinder" %% "dispatch-nio" % "0.8.3" withSources()
 val specs = "org.scala-tools.testing" %% "specs" % "1.6.8" % "test"
-val log_lib = "ch.qos.logback" % "logback-classic" % "0.9.26"
+
+val renalias_repo = "renalias.net Repository" at "http://phunkphorce.github.com/maven"
+val common_lib = "net.renalias" %% "scala-common" % "20110712" withSources()
 
 }
